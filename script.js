39879de51f38369e53a41f6f3c7cc80d7c7c50e6
FourIFreaky <ponton.justin@gmail.com>
2022-02-23 21:10:15 +0000

    card deck api

---
+++ script.js
@@ -0,0 +1,50 @@
+let deckName = "";
+let hand = "";
+document.getElementById("newDeckButton").addEventListener("click", function(event)
+	{
+		event.preventDefault();
+		const url1 = "https://deckofcardsapi.com/api/deck/new/";
+		fetch(url1)
+			.then(function(response) {
+				return response.json();
+			}).then(function(json) {
+				deckName = json.deck_id;
+				console.log(deckName);
+				hand = "";
+				document.getElementById("yourHand").innerHTML = "";
+			});
+	});
+document.getElementById("shuffleButton").addEventListener("click", function(event)
+	{
+		event.preventDefault();
+		const url2 = "https://deckofcardsapi.com/api/deck/" + deckName + "/shuffle/?remaining+true";
+		fetch(url2)
+			.then(function(response) {
+				return response.json();
+			}).then(function(json) {
+				return json;
+			});
+	});
+document.getElementById("drawButton").addEventListener("click", function(event)
+	{
+		event.preventDefault();
+		const value = document.getElementById("drawNumber").value;
+		if(value === "")
+			return;
+		//if(value >== 52 || value === 0)
+		//{
+		//	return;
+		//}
+		console.log(value);
+		const url3 = "https://deckofcardsapi.com/api/deck/" + deckName + "/draw/?count=" + value;
+		fetch(url3)
+			.then(function(response) {
+				return response.json();
+			}).then(function(json) {
+				hand = "";
+				for(let i = 0; i < json.cards.length; i++) {
+					hand += '<img src="https://deckofcardsapi.com/static/img/' + json.cards[i].code + '.png"/>';
+				}
+				document.getElementById("yourHand").innerHTML += hand; 
+			});
+	});
