81729e470f5f5133e00c577aad9cc6d622e4a1bc
Justin Ponton <ponton.justin@gmail.com>
2022-03-16 19:42:37 -0600

    Vue Number Grid Game

--- script.js
+++ script.js
@@ -1,46 +1,43 @@
 let deckName = "";
 let hand = "";
-document.getElementById("newDeckButton").addEventListener("click", function(event)
-	{
-		event.preventDefault();
-		const url1 = "https://deckofcardsapi.com/api/deck/new/";
-		fetch(url1)
-			.then(function(response) {
-				return response.json();
-			}).then(function(json) {
-				deckName = json.deck_id;
-				console.log(deckName);
-				hand = "";
-				document.getElementById("yourHand").innerHTML = "";
-			});
-	});
-document.getElementById("shuffleButton").addEventListener("click", function(event)
-	{
-		event.preventDefault();
-		const url2 = "https://deckofcardsapi.com/api/deck/" + deckName + "/shuffle/?remaining+true";
-		fetch(url2)
-			.then(function(response) {
-				return response.json();
-			}).then(function(json) {
-				return json;
-			});
-	});
-document.getElementById("drawButton").addEventListener("click", function(event)
-	{
-		event.preventDefault();
-		const value = document.getElementById("drawNumber").value;
-		if(value === "") { return; }
-		if(parseInt(value) > 52 || parseInt(value) == 0) { return; }
-		console.log(value);
-		const url3 = "https://deckofcardsapi.com/api/deck/" + deckName + "/draw/?count=" + value;
-		fetch(url3)
-			.then(function(response) {
-				return response.json();
-			}).then(function(json) {
-				hand = "";
-				for(let i = 0; i < json.cards.length; i++) {
-					hand += '<div id="oneCard"><img src="https://deckofcardsapi.com/static/img/' + json.cards[i].code + '.png"/></div>';
-				}
-				document.getElementById("yourHand").innerHTML += hand; 
-			});
-	});
+document.getElementById("newDeckButton").addEventListener("click", function (event) {
+	event.preventDefault();
+	const url1 = "https://deckofcardsapi.com/api/deck/new/";
+	fetch(url1)
+		.then(function (response) {
+			return response.json();
+		}).then(function (json) {
+			deckName = json.deck_id;
+			console.log(deckName);
+			hand = "";
+			document.getElementById("yourHand").innerHTML = "";
+		});
+});
+document.getElementById("shuffleButton").addEventListener("click", function (event) {
+	event.preventDefault();
+	const url2 = "https://deckofcardsapi.com/api/deck/" + deckName + "/shuffle/?remaining+true";
+	fetch(url2)
+		.then(function (response) {
+			return response.json();
+		}).then(function (json) {
+			return json;
+		});
+});
+document.getElementById("drawButton").addEventListener("click", function (event) {
+	event.preventDefault();
+	const value = document.getElementById("drawNumber").value;
+	if (value === "") { return; }
+	if (parseInt(value) > 52 || parseInt(value) == 0) { return; }
+	console.log(value);
+	const url3 = "https://deckofcardsapi.com/api/deck/" + deckName + "/draw/?count=" + value;
+	fetch(url3)
+		.then(function (response) {
+			return response.json();
+		}).then(function (json) {
+			hand = "";
+			for (let i = 0; i < json.cards.length; i++) {
+				hand += '<div id="oneCard"><img src="https://deckofcardsapi.com/static/img/' + json.cards[i].code + '.png"/></div>';
+			}
+			document.getElementById("yourHand").innerHTML += hand;
+		});
+});
